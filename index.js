b4964f397965efd66835024d861e26e916bb4297
Mark Rogowsky <65688548+markrogo@users.noreply.github.com>
2020-06-18 14:55:40 -0700

    Initial commit

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
-
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30;
+const name = "Mark";
 
 
 
@@ -14,6 +17,9 @@
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
+
+let monthlyInterestRate = interestRate / 12;
+let periods = years*12;
 
 
 
@@ -35,7 +41,10 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
-
+let n1 = (1 + monthlyInterestRate )^N;
+let numerator = p * n1 * monthlyInterestRate;
+let denominator = n1 - 1;
+let monthlyRate = numerator / denominator;
 
 
 // 🏡 Task 3: Function
@@ -44,6 +53,10 @@ When your math is correct, monthlyRate will equal 1073.64
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
 
+mortgageCalculator (name) {
+    
+    console.log (name + " your monthly rate is " + monthlyRate);
+}
 
 
 
